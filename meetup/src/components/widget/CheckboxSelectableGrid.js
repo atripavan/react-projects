2c410a40c5059f48840bec114c309bc689414200
Pavan Atri <atripavan@gmail.com>
2023-03-27 08:41:20 +0530

    Added user selection to meetup

--- meetup/src/components/widget/CheckboxSelectableGrid.js
+++ meetup/src/components/widget/CheckboxSelectableGrid.js
@@ -1,87 +1,73 @@
 import React, { useState } from "react";
 import ReactDataGrid from "react-data-grid";
+import classes from "./CheckboxSelectableGrid.module.css";
+
 
 function CheckboxSelectableGrid(props) {
-  const columns = props.cols;
-
   const rows = props.rows;
+  const [gridData, setGridData] = useState(rows);
 
-  const CheckboxSelector = ({ row, onCheckboxChange }) => {
-    const [isChecked, setIsChecked] = useState(false);
-
-    const handleCheckboxChange = () => {
-      setIsChecked(!isChecked);
-      onCheckboxChange(row, !isChecked);
-    };
-
-    return (
-      <input
-        type="checkbox"
-        checked={isChecked}
-        onChange={handleCheckboxChange}
-      />
-    );
+  const handleCheckboxChange = (event, index) => {
+    const newData = [...gridData];
+    newData[index].selected = event.target.checked;
+    setGridData(newData);
   };
 
-  const CheckboxSelectorHeader = ({ rows, onCheckboxChange }) => {
-    const [isChecked, setIsChecked] = useState(false);
+  const handleHeaderCheckboxChange = (event) => {
+    const newData = [...gridData];
 
-    const handleCheckboxChange = () => {
-      setIsChecked(!isChecked);
-      onCheckboxChange(rows, !isChecked);
-    };
-
-    return (
-      <input
-        type="checkbox"
-        checked={isChecked}
-        onChange={handleCheckboxChange}
-      />
+    gridData.map(
+      (item, index) => (newData[index].selected = event.target.checked)
     );
+    setGridData(newData);
   };
 
-    const [selectedRows, setSelectedRows] = useState([]);
+  const handleSubmit = () => {
+    props.handleGridSubmit(gridData);
+  };
 
-    const handleCheckboxChange = (row, isChecked) => {
-      if (isChecked) {
-        setSelectedRows([...selectedRows, row]);
-      } else {
-        setSelectedRows(
-          selectedRows.filter((selectedRow) => selectedRow.id !== row.id)
-        );
-      }
-    };
-
-    const handleHeaderCheckboxChange = (rows, isChecked) => {
-      if (isChecked) {
-        setSelectedRows([...selectedRows, ...rows]);
-      } else {
-        setSelectedRows(
-          selectedRows.filter(
-            (selectedRow) => !rows.find((row) => row.id === selectedRow.id)
-          )
-        );
-      }
-    };
-
-    return (
-      <ReactDataGrid
-        columns={[
-          {
-            key: "selector",
-            name: "",
-            formatter: CheckboxSelector,
-            headerRenderer: CheckboxSelectorHeader,
-          },
-          ...columns,
-        ]}
-        rows={rows}
-        rowKey="id"
-        onSelectedRowsChange={setSelectedRows}
-        selectedRows={selectedRows}
-        enableCellSelect={false}
-      />
-    );
+  return (
+    <div className={classes.popupbox}>
+      <div className={classes.box}>
+        <span className={classes.closeicon} onClick={props.handleClose}>x</span>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                <input
+                  id="hdrChkbox"
+                  type="checkbox"
+                  onChange={(event) => handleHeaderCheckboxChange(event)}
+                />
+              </th>
+              <th>ID</th>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {gridData.map((item, index) => (
+              <tr key={item.userId}>
+                <td>
+                  <input
+                    type="checkbox"
+                    checked={item.selected}
+                    onChange={(event) => handleCheckboxChange(event, index)}
+                  />
+                </td>
+                <td>{item.userId}</td>
+                <td>{item.name}</td>
+              </tr>
+            ))}
+            <tr>
+              <td colSpan={2}>
+                <button onClick={handleSubmit}>Submit</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default CheckboxSelectableGrid;
